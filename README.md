99fc97bcdaa38d6ec01a06a70b6acb6eab1a533d
Bilal Karim <karimbilal156@gmail.com>
2023-03-04 14:10:48 +0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue-todos
+
+Vue Todos is a CRUD Todo application.
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/views/AboutView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container about">
+    <h1>Vue Todos</h1>
+    <p>
+      Vue Todos is a CRUD Todo tracking application.
+    </p>
+    <p>
+      Track your daily task's using this todo tracker with abilities to save, complete, edit & delete todos.
+    </p>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@media (min-width: 1024px) {
+  .about {
+    padding: 25px 16px;
+
+    h1 {
+      margin-bottom: 15px;
+    }
+
+    p {
+      margin-bottom: 12px
+    }
+  }
+}
+</style>
